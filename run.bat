ad4a38963557ed4e21144ba295d88f3fc1a86e8b
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2024-08-09 14:03:01 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-REM v1.3.1
+REM v1.3.2
 
 : fleasion by @cro.p
 : distributed in https://discord.gg/v9gXTuCz8B
@@ -10,8 +10,7 @@ REM v1.3.1
 : Check if the latest version of Python is installed and install if necessary
 
 
-set LM=True
-set CU=True
+set LM=True & set CU=True
 reg Query "HKLM\SOFTWARE\Python\PythonCore\3.12" /v "Version" | find "3.12.4" || set LM=False
 reg Query "HKCU\SOFTWARE\Python\PythonCore\3.12" /v "Version" | find "3.12.4" || set CU=False
 if %LM% == True goto pip
@@ -23,7 +22,7 @@ cls
 echo Downloading python...
 curl -sSL -o python.exe https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe
 echo Installing..
-python.exe /quiet InstallAllUsers=1 PrepemdPath=1 Include_test=0 Include_doc=0
+python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_doc=0
 del python.exe >nul
 
 
@@ -34,7 +33,7 @@ del python.exe >nul
 cls
 echo Updating/checking for pip..
 py -m pip install --upgrade pip >nul 2>&1
-if %errorlevel%==9009 (
+if %errorlevel%==1 (
     cls
     echo Downloading pip...
     curl -sSL -o get-pip.py https://bootstrap.pypa.io/get-pip.py
@@ -42,6 +41,7 @@ if %errorlevel%==9009 (
     py get-pip.py --no-setuptools --no-wheel >nul 2>&1
     del get-pip.py
 )
+if %errorlevel%==9009 goto error
 
 cls
 echo Installing/checking for requests package...
@@ -52,8 +52,15 @@ py -m pip install requests >nul 2>&1
 
 
 cls
-if exist %cd%\fleasion.py py %cd%\fleasion.py & exit /b else goto install
-
-:install
+if exist %cd%\fleasion.py goto launch
 curl -sSL -o %cd%\fleasion.py https://raw.githubusercontent.com/CroppingFlea479/Fleasion/main/fleasion.py --ssl-no-revoke
-py %cd%\fleasion.py
+
+:launch
+py %cd%\fleasion.py if %errorlevel%==9009 goto error
+exit /b
+
+:error
+echo x=msgbox("Your Python installation either failed or isn't added to PATH." + vbCrLf + " " + vbCrLf + "This is usually fixable by just restarting the 'run.bat' file or right clicking it and running is as Administrator."  + vbCrLf + " " + vbCrLf + "Please uninstall Python by searching 'Add or remove programs', pressing enter, finding Python and removing every installation you find one by one and rerun the run.bat file." + vbCrLf + " " + vbCrLf + "If Python fails to uninstall, download Bull Crap Uninstaller and uninstall Python by right clicking and selecting the 'Uninstall Manually' option.", 16, "Fleasion dependency setup failed") > %cd%\error.vbs
+cscript //nologo %cd%\error.vbs
+del error.vbs
+exit
