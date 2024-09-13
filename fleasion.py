a2561e8d9d5f85ed0fd068c2d6803ca89c43a52b
yoloblokers <kafiro13@gmail.com>
2024-09-13 13:31:19 +1000

    Update fleasion.py
    Cache checking should be operational again - known issues: model hashes are broken.

--- fleasion.py
+++ fleasion.py
@@ -1,4 +1,4 @@
-# v1.8.8
+# v1.8.9
 # Fleasion, open sourced cache modifier made by @cro.p, intended for Phantom Forces. plz dont abuse D:
 # discord.gg/v9gXTuCz8B
 
@@ -220,10 +220,10 @@ else:
     print(f"Unsupported OS - {os}")
     exit()
 
-mod_cache = True
-pf_cache = True
+mod_cache = False
+pf_cache = False
 
-mod_cache_check_path = os.path.join(folder_path, '016a313606e2f99a85bb1a91083206fc')
+mod_cache_check_path = os.path.join(folder_path, '29ec14d6f908cabca7fae131487d96d8')  # 016a313606e2f99a85bb1a91083206fc
 pf_cache_check_path = os.path.join(folder_path, '7b8ca4a4ec7addd0f55179a86e49a5a1' if os_name == 'Linux' else '8a7090ac9b2e858f4aee9e19a0bfd562')
 
 if os.path.exists(mod_cache_check_path): mod_cache = True
